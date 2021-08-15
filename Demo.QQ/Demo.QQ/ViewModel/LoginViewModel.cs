7dbba714e49ea8cfe6a79d7f2830fb0087c024b8
yanleicf <yanleicf@163.com>
2021-08-15 22:18:07 +0800

    no message

--- Demo.QQ/Demo.QQ/ViewModel/LoginViewModel.cs
+++ Demo.QQ/Demo.QQ/ViewModel/LoginViewModel.cs
@@ -11,6 +11,7 @@ namespace Demo.QQ.ViewModel
     public class LoginViewModel
     {
         public CommandBase CloseWindowCommand { get; set; }
+        public CommandBase MiniWindowCommand { get; set; }
 
         public LoginViewModel()
         {
@@ -23,6 +24,16 @@ namespace Demo.QQ.ViewModel
                 return true;
             };
             CloseWindowCommand = new CommandBase(action1, func1);
+
+            Action<object> action2 = (o) =>
+            {
+                (o as Window).WindowState = WindowState.Minimized;
+            };
+            Func<object, bool> func2 = (o) =>
+            {
+                return true;
+            };
+            MiniWindowCommand = new CommandBase(action2, func2);
         }
     }
 }
